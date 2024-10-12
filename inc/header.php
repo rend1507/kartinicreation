af8ab1194ca31c4ffd4ad68c7c397c97558fd246
Rendy Yofana Kusuma <rendyyofanakusuma@gmail.com>
2024-10-12 16:33:34 +0700

    Add icon Fonts embed

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,25 @@
 <html>
-    <head>
-          <link rel="stylesheet" href="src/dist/bootstrap/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="src/dist/aos/aos.css" />
 
-          <!-- Custom -->
-          <link rel="stylesheet" href="src/css/style.css" />
-    </head>
-    <body>
-    
+<head>
+      <!-- Icon -->
+      <link rel="icon" type="image/jpg" href="src/img/icon-kartini-square-bg.jpg" />
+       
+
+      <link rel="stylesheet" href="src/dist/bootstrap/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="src/dist/aos/aos.css" />
+
+      <!-- Custom -->
+      <link rel="stylesheet" href="src/css/style.css" />
+</head>
+
+<body class="d-flex flex-column h-100">
+
+      <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                  <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                  <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+                  <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+                  <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                  <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            </ul>
+      </header>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 include("inc/header.php");
 ?>
+
+<h1>Test Header</h1>
+
 Test
 <?php
 include("inc/footer.php");
